daef4dbac3be0374e03d766516512544ebe1a910
mahererais <raismaher.store@gmail.com>
2023-08-25 00:23:03 +0200

    add route login/register on header navBar + update login page style

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -15,7 +15,7 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 class RegistrationController extends AbstractController
 {
     /**
-     * @Route("/register", name="app_register")
+     * @Route("/register", name="tcb_front_security_register")
      */
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
