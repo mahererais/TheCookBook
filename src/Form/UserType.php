d49ca040e64817df317c597acc235f4570e871da
mahererais <raismaher.store@gmail.com>
2023-08-29 00:06:15 +0200

    template profile form (no style yet)

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,7 +6,9 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -37,7 +39,11 @@ class UserType extends AbstractType
                     "placeholder" => "Entrez votre nom"
                 ]
             ])
-            ->add('picture')
+            ->add('picture', FileType::class, [
+                "label" => "Image",
+                "mapped" => false, // unmapped means that this field is not associated to any entity property
+                "required" => false // make it optional so you don't have to re-upload the PDF file every time you edit user profile
+                ])
             ->add('speciality', TextType::class, [
                 "label" => "Specialité",
                 "attr" => [
@@ -54,7 +60,8 @@ class UserType extends AbstractType
             ->add('presentation', TextareaType::class, [
                 "label" => "Presentation",
                 "attr" => [
-                    "placeholder" => "Entrez ici une presentation"
+                    "placeholder" => "Entrez ici une presentation",
+                    "rows" => 5,
                 ]
             ])
             ->add('password', PasswordType::class, [
@@ -66,8 +73,8 @@ class UserType extends AbstractType
             ->add('status', ChoiceType::class, [
                 "label" => "Status",
                 "choices" => [
-                    "Private" => "Privé",
-                    "Public" => "Public",
+                    "Private" => "privé",
+                    "Public" => "public",
                 ],
                 "expanded" => true,
             ]);
