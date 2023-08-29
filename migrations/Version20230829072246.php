72e59273490e986233b23da8b2bcb37e1b48c6d9
simon-aj-thomas <simon.thomas@oclock.school>
2023-08-29 09:33:09 +0200

    update on search form on all pages

--- migrations/Version20230829072246.php
+++ migrations/Version20230829072246.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230828154608 extends AbstractMigration
+final class Version20230829072246 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,18 +20,20 @@ final class Version20230828154608 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE category (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(64) NOT NULL, slug VARCHAR(64) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE recipe (id INT AUTO_INCREMENT NOT NULL, category_id INT NOT NULL, user_id INT NOT NULL, title VARCHAR(128) NOT NULL, picture VARCHAR(255) NOT NULL, steps LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', updated_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', status VARCHAR(64) NOT NULL, slug VARCHAR(128) NOT NULL, duration INT DEFAULT NULL, ingredients LONGTEXT NOT NULL COMMENT \'(DC2Type:array)\', ebook TINYINT(1) NOT NULL, INDEX IDX_DA88B13712469DE2 (category_id), INDEX IDX_DA88B137A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, firstname VARCHAR(64) NOT NULL, lastname VARCHAR(64) DEFAULT NULL, picture VARCHAR(255) DEFAULT NULL, speciality VARCHAR(64) DEFAULT NULL, experience LONGTEXT DEFAULT NULL, presentation LONGTEXT DEFAULT NULL, status VARCHAR(64) NOT NULL, slug VARCHAR(64) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE recipe ADD user_id INT NOT NULL, ADD ebook TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE recipe ADD CONSTRAINT FK_DA88B13712469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
         $this->addSql('ALTER TABLE recipe ADD CONSTRAINT FK_DA88B137A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
-        $this->addSql('CREATE INDEX IDX_DA88B137A76ED395 ON recipe (user_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE recipe DROP FOREIGN KEY FK_DA88B13712469DE2');
         $this->addSql('ALTER TABLE recipe DROP FOREIGN KEY FK_DA88B137A76ED395');
+        $this->addSql('DROP TABLE category');
+        $this->addSql('DROP TABLE recipe');
         $this->addSql('DROP TABLE user');
-        $this->addSql('DROP INDEX IDX_DA88B137A76ED395 ON recipe');
-        $this->addSql('ALTER TABLE recipe DROP user_id, DROP ebook');
     }
 }
