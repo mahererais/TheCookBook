526a10880ea8e3307b0d703969a02942b07162f9
Manuellacosnard <cosnardmanuella@gmail.com>
2023-08-31 11:40:54 +0200

    first upload on cloudinary with widget (button in templates/Admin/category/form) OK but cloudname in script git branch

--- src/Service/CloudinaryService.php
+++ src/Service/CloudinaryService.php
@@ -6,23 +6,42 @@ use Cloudinary\Api\Upload\UploadApi;
 use Cloudinary\Cloudinary;
 use Cloudinary\Transformation\Format;
 use Cloudinary\Transformation\Resize;
+use Cloudinary\Configuration\Configuration;
+
 
 class CloudinaryService
 {
-    private $apiKey;
+       
+    private $apiSecret;
+    private $apiMyKey;
+    private $apiCloud;
 
-    public function __construct(string $apiKey)
+    public function __construct(string $apiSecret, string $apiMyKey, string $apiCloud)
     {
-        $this->apiKey = $apiKey;
+        // Configuration::instance([
+        // 'cloud' => [
+        //     'cloud_name' => $apiCloud, 
+        //     'api_key' => $apiMyKey, 
+        //     'api_secret' => $apiSecret],
+        //     'url' => [ 
+        //         'secure' => true
+        //         ]
+        //     ]
+        // );
+
+        $this->apiMyKey = $apiMyKey;
+        $this->apiSecret = $apiSecret;
+        $this->apiCloud = $apiCloud;
     }
 
 
-    public function upload(Cloudinary $cloudinary)
+    public function upload()
     {
         // Upload the image
         // $upload = new UploadApi();
-        $cloudinary = new Cloudinary();
-        $cloudinary->uploadApi->upload('my_image.jpg');
+        // $cloudinary = new Cloudinary();
+        // $cloudinary->uploadApi->upload('assets/images/user_profile_default.png');
+
         // echo '<pre>';
         // echo json_encode(
         //     $upload->upload('https://res.cloudinary.com/demo/image/upload/flower.jpg', [
@@ -32,6 +51,6 @@ class CloudinaryService
         //     JSON_PRETTY_PRINT
         // );
         // echo '</pre>';
-        dd($cloudinary);
+        // dd($cloudinary);
     }
 }
