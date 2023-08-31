094cb40a7c5e13cdbb36dc197c22b5c9f61199a5
mariebourhis <marie.bourhis1@gmail.com>
2023-08-31 15:56:07 +0200

    access denied for a logged user to another profile

--- src/Security/Voter/ProfileVoter.php
+++ src/Security/Voter/ProfileVoter.php
@@ -1,44 +1,31 @@
 <?php
 
-namespace App\Security\Voter;
+namespace App\src\Security\Voter;
 
+use App\Entity\User;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class ProfileVoter extends Voter
 {
-    public const EDIT = 'POST_EDIT';
-    public const VIEW = 'POST_VIEW';
+    public const ACCESS = 'PROFILE_ACCESS';
 
     protected function supports(string $attribute, $subject): bool
     {
-        // replace with your own logic
-        // https://symfony.com/doc/current/security/voters.html
-        return in_array($attribute, [self::EDIT, self::VIEW])
-            && $subject instanceof \App\Entity\Profile;
+        return $attribute === self::ACCESS && $subject instanceof User;
     }
 
     protected function voteOnAttribute(string $attribute, $subject, TokenInterface $token): bool
     {
         $user = $token->getUser();
-        // if the user is anonymous, do not grant access
+
         if (!$user instanceof UserInterface) {
+            // L'utilisateur n'est pas connecté
             return false;
         }
 
-        // ... (check conditions and return true to grant permission) ...
-        switch ($attribute) {
-            case self::EDIT:
-                // logic to determine if the user can EDIT
-                // return true or false
-                break;
-            case self::VIEW:
-                // logic to determine if the user can VIEW
-                // return true or false
-                break;
-        }
-
-        return false;
+        // Si l'utilisateur connecté est le même que le profil demandé
+        return $user === $subject;
     }
 }
