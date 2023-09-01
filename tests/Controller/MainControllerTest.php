577afa205364fb04110f113a517dc9a544555783
Oumarsy8 <oumar.sy@oclock.school>
2023-09-01 16:38:31 +0200

    MainControllerTest ok

---
+++ tests/Controller/MainControllerTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Tests\Controller\UserRepository;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MainControllerTest extends WebTestCase
+{
+    public function testHomeAnonyme(): void
+    {
+        // equivaut à créer un "faux" navigateur
+        $client = static::createClient();
+
+        // On crée une requête sur la racine du site
+        $crawler = $client->request('GET', '/');
+
+
+        // Est ce que la requête a bien renvoyé une reponse 200
+        $this->assertResponseIsSuccessful();
+
+        // Je vérifie qu'il y a bien le lien de login dans la page 
+        $this->assertSelectorExists("a[href='/login']", "Le lien de connexion n'est pas visible");
+
+
+    }
+
+   
+}
