170a7d177015f6253e675b6c21d35fff6e9010b8
Oumarsy8 <oumar.sy@oclock.school>
2023-09-01 17:47:20 +0200

    RecipeControllerTest ok

--- tests/Controller/MainControllerTest.php
+++ tests/Controller/MainControllerTest.php
@@ -24,5 +24,7 @@ class MainControllerTest extends WebTestCase
 
     }
 
+    
+
    
 }
---
+++ tests/Controller/RecipeControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RecipeControllerTest extends WebTestCase
+{
+    public function testGetAll()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/recipes');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertSelectorTextContains('h1', 'Liste des recettes'); // Remplacez 'Recipes' par le texte attendu dans votre modèle
+    }
+    
+}
