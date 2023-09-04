d3382d2ced94c4db6cb112c252b90b92b8fdeda4
simon-aj-thomas <simon.thomas@oclock.school>
2023-09-04 11:17:11 +0200

    update on chefs template: aligning see more button

--- migrations/Version20230904084017.php
+++ migrations/Version20230904084017.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230903193320 extends AbstractMigration
+final class Version20230904084017 extends AbstractMigration
 {
     public function getDescription(): string
     {
