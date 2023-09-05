ee963ec326ad51d935908fffc71c28690d0bccf3
mariebourhis <marie.bourhis1@gmail.com>
2023-09-05 14:30:55 +0200

    RecipeListener modified to create a new slug for all the same recipes

--- src/Listener/CategoryListener.php
+++ src/Listener/CategoryListener.php
@@ -3,6 +3,7 @@
 namespace App\Listener;
 
 use App\Entity\Category;
+use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class CategoryListener
@@ -11,13 +12,16 @@ class CategoryListener
     private $slugger;
 
     public function __construct(SluggerInterface $slugger){
+        // injection of the SluggerInterface service in the listener
         $this->slugger = $slugger;
     }
 
-    public function slugifyTitle(Category $category){
-
-        $category->setSlug($this->slugger->slug($category->getTitle()));
-
+    public function slugifyTitle(Category $category)
+    {
+        // Génère un slug à partir du titre et l'attribue à la catégorie
+        $slug = $this->slugger->slug($category->getTitle())->lower();
+        $category->setSlug($slug);
     }
+
 
 }
--- src/Listener/RecipeListener.php
+++ src/Listener/RecipeListener.php
@@ -3,21 +3,59 @@
 namespace App\Listener;
 
 use App\Entity\Recipe;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class RecipeListener
 {
 
     private $slugger;
+    private $entityManager;
 
-    public function __construct(SluggerInterface $slugger){
+    public function __construct(SluggerInterface $slugger, EntityManagerInterface $entityManager)
+    {
+        // injecting the slugger service in the listener + entityManager
         $this->slugger = $slugger;
+        $this->entityManager = $entityManager;
     }
 
-    public function slugifyTitle(Recipe $recipe){
+    public function slugifyTitle(Recipe $recipe)
+    {
+        // creating an initial slug from the title
+        $originalSlug = $this->slugger->slug($recipe->getTitle())->lower();
+        $slug = $originalSlug;
 
-        $recipe->setSlug($this->slugger->slug($recipe->getTitle())); // !concaténation avec un élément unique
+        // check if the slug alread exists in the data base
+        $existingSlugs = $this->getExistingSlugs($recipe);
+        $counter = 1;
 
+        // loop until a unique slug has been found
+        while (in_array($slug, $existingSlugs)) {
+            // incrementing the slug with a +1
+            $slug = $originalSlug . '-' . $counter;
+            $counter++;
+        }
+        // definition of the slug with a unique slug
+        $recipe->setSlug($slug);
+    }
+
+    private function getExistingSlugs(Recipe $recipe)
+    {
+
+        $existingSlugs = [];
+        $title = $recipe->getTitle();
+
+        // Looking for recipes with the same title
+        $existingRecipes = $this->entityManager
+            ->getRepository(Recipe::class)
+            ->findBy(['title' => $title]);
+
+        // Collecte des slugs existants
+        foreach ($existingRecipes as $existingRecipe) {
+            $existingSlugs[] = $existingRecipe->getSlug();
+        }
+
+        return $existingSlugs;
     }
 
 }
